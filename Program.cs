e97328fc6dde75691ee5e5036fd0abd1d21c14ca
SIFAQES <siphaxzer@gmail.com>
2023-09-28 13:35:21 +0200

    app calculadora

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace Entornos_escritorio_practica1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new calcoladora());
+
         }
     }
 }
